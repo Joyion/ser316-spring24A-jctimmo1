47b991dd5320a952cf5b5eed52d9f68afcfa2a0d
Joyion Timmons <joyiont@gmail.com>
2024-06-08 13:53:36 -0400

    Add whitebox tests for countCorrectLetters

--- src/test/java/GivenWhiteBox.java
+++ src/test/java/GivenWhiteBox.java
@@ -1,5 +1,6 @@
 import static org.junit.Assert.*;
 
+import org.junit.Assert;
 import org.junit.Before;
 
 import org.junit.Test;
@@ -17,5 +18,34 @@ public class GivenWhiteBox {
         Game game = new Game("lion", "Dr. M");
         assertEquals(10, game.getPoints());
     }
+
+    // Test Case 2
+    @Test
+    public void countCorrectLetters_WithOneCorrectChar_Test() {
+        game = new Game("James", "James");
+        game.makeGuess("a");
+        int c = game.countCorrectLetters();
+        assert(c == 1);
+    }
+
+    // Test Case 3
+    @Test
+    public void noCorrectLetters_withNoCorrectChar_Test() {
+        game = new Game("James", "James");
+        game.makeGuess("a");
+        int c = game.countCorrectLetters();
+        assert(c == 0);
+    }
+
+    // Test Case 1 and 4
+    @Test
+    public void noCorrectLetters_withManyCorrectChar_Test() {
+        game = new Game("Babboon", "James");
+        game.makeGuess("b");
+        game.makeGuess("c");
+        int c = game.countCorrectLetters();
+        assert(c == 2);
+    }
+
 
 }
